5959e8db95435698d24a3d6e63c96de5e7174e37
Astrid <eyerees.main@gmail.com>
2026-03-21 20:29:24 +0100

    TILESET/CONFINERS

---
+++ Assets/MapTransition.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] private PolygonCollider2D mapBoundary; 
+    
+    [SerializeField] private Direction direction; 
+    [SerializeField] private float transitionDistance = 2f;
+
+    private CinemachineConfiner2D confiner; 
+
+    public enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    private void Awake()
+    {
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundary;
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += transitionDistance; 
+                break;
+            case Direction.Down:
+                newPos.y -= transitionDistance; 
+                break;
+            case Direction.Left:
+                newPos.x -= transitionDistance; 
+                break;
+            case Direction.Right:
+                newPos.x += transitionDistance; 
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+}
